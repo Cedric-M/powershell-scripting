ab5d49250b949094306d25fc2e39d8ffb971840e
Cedric-M <cirdec95@gmail.com>
2019-12-08 20:17:44 +0100

    Add: Install-Firefox

---
+++ Install-Firefox.ps1
@@ -0,0 +1,4 @@
+$Path = $env:TEMP; $Installer = "chrome_installer.exe"; 
+Invoke-WebRequest "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-GB" -OutFile $Path\$Installer; 
+Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; 
+Remove-Item $Path\$Installer
