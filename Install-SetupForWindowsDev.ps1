50ee4c3827afd324f21afa47244094ebef25ac9b
Cedric-M <cirdec95@gmail.com>
2019-12-16 19:33:21 +0100

    Fixed WebRequest

--- Install-SetupForWindowsDev.ps1
+++ Install-SetupForWindowsDev.ps1
@@ -8,14 +8,12 @@ Remove-Item $Path\$Installer
 
 Write-Host "Downloading Git for Windows" -ForegroundColor DarkGreen
 $Installer = "git_installer.exe";
-Invoke-WebRequest "https://git-scm.com/download/win" -OutFile $Path\$Installer;
+Invoke-WebRequest "https://github.com/git-for-windows/git/releases/download/v2.24.1.windows.2/Git-2.24.1.2-64-bit.exe" -OutFile $Path\$Installer;
 Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
 Remove-Item $Path\$Installer
 
 Write-Host "Downloading Git Kraken" -ForegroundColor DarkGreen
 $Installer = "gitkraken_installer.exe";
-Invoke-WebRequest "https://www.gitkraken.com/download/windows64" -OutFile $Path\$Installer;
+Invoke-WebRequest "https://release.gitkraken.com/win64/GitKrakenSetup.exe" -OutFile $Path\$Installer;
 Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait;
 Remove-Item $Path\$Installer
-
-Read-Host -Prompt "Press Enter to exit"
